b8cf6d26eb134a5e72b95e848f96e35440573057
haopham1705 <phamvanhao009@gmail.com>
2021-11-13 11:25:52 +0700

    instead nullist on cardresult

--- src/components/CardResult/index.jsx
+++ src/components/CardResult/index.jsx
@@ -7,11 +7,11 @@ function CardResult({ electoralNumber, avatar, namePresident, votesNumber }) {
             <div class="card">
                 <img src={avatar} alt="Avatar" className="card__avatar" />
                 <div class="card__container">
-                    <h2 className="card__name"><b>{namePresident || 'John Doe'}</b></h2>
+                    <h2 className="card__name"><b>{namePresident ?? 'John Doe'}</b></h2>
                     <p>Electoral College</p>
-                    <h1 className="card__electoral-number">{electoralNumber || '123'}</h1>
+                    <h1 className="card__electoral-number">{electoralNumber ?? '123'}</h1>
                     <p>Votes</p>
-                    <p className="card__votes-number">{votesNumber || '123123'}</p>
+                    <p className="card__votes-number">{votesNumber ?? '123123'}</p>
                 </div>
             </div>
         </>
